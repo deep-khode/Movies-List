228a52ac858a76238db0c939a6d2856a1f4310fe
deep-khode <100463989+deep-khode@users.noreply.github.com>
2022-05-08 23:15:10 +0530

    Added Loading

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -7,7 +7,7 @@ export class SearchMovies extends Component {
   
   constructor() {
     super();
-    this.state = {movieResult: [], searchKeyword: ''};
+    this.state = {movieResult: [], searchKeyword: '', resultFound: true, Loading: true};
   }
 
   componentDidMount(){
@@ -17,24 +17,25 @@ export class SearchMovies extends Component {
   runAPI = () => {
     axios.get("https://api.themoviedb.org/3/movie/popular?api_key=cfe422613b250f702980a3bbf9e90716")
     .then((res) => {
-        console.log(res.data.results)
-        this.setState({movieResult: res.data.results})
+        this.setState({movieResult: res.data.results, Loading: false,resultFound: true})
     }).catch((err) => {
         console.log(err);
+        this.setState({Loading: false,resultFound: false})
     })
   }
 
   handleInput = (input) => {
-
+    this.setState({Loading: true})
     if(input == ''){
         this.runAPI();
     }
     
     axios.get(`https://api.themoviedb.org/3/search/movie?query=${input}&api_key=cfe422613b250f702980a3bbf9e90716`)
     .then((res) => {
-        this.setState({movieResult: res.data.results})
+        this.setState({movieResult: res.data.results, Loading: false,resultFound: true})
     }).catch((err) => {
         console.log(err);
+        this.setState({Loading: false,resultFound: false})
     })
     this.setState({searchKeyword: input})
     
@@ -50,21 +51,23 @@ export class SearchMovies extends Component {
             <div>
                 <input type="search" id="searchInput" value={this.state.searchKeyword} name="searchKeyword" onChange={(e) => this.handleInput(e.target.value)} />
             </div>
-            <div id="result">
-                {
-                    this.state.movieResult.map((movie, index) => {
-                        return <>
-                            <div className="card" key={index} title={movie.original_title} >
-                                <img className="image" src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2/${movie.poster_path}`} alt={movie.original_title} />
-                                <h3 className="title">{movie.original_title}</h3>
-                            </div>
-                        </>
-                    })
-                }
-                <div>
-
+            {
+                this.state.Loading ?  'Loding...' :  <div id="result">
+                        {
+                            this.state.movieResult.map((movie, index) => {
+                                return <>
+                                    <div className="card" key={index} title={movie.original_title} >
+                                        <img className="image" src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2/${movie.poster_path}`} alt={movie.original_title} />
+                                        <h3 className="title">{movie.original_title}</h3>
+                                    </div>
+                                </>
+                            })
+                        }
+                        <div>
+                    </div>
                 </div>
-            </div>
+            }
+            
             <footer id="footer">
                 <p>© 2022 Deep Khode ✌️</p>
             </footer>
